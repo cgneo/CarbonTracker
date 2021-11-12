d30118a0611393bc65fa512e1b1325b563c7189c
nina.bilirossi <nina.bilirossi@gmail.com>
2021-11-12 11:31:37 +0100

    cleaning up the file and adding widgets in thr

--- GUI/Graphs (Jane and Nina)/graphs_nina/main.cpp
+++ GUI/Graphs (Jane and Nina)/graphs_nina/main.cpp
@@ -1,6 +1,5 @@
 #include "mainwindow.h"
-import QtCharts 2.15
-#include <QAbstractSeries>
+
 #include <QApplication>
 
 int main(int argc, char *argv[])
@@ -11,9 +10,4 @@ int main(int argc, char *argv[])
     return a.exec();
 }
 
-QLineSeries* series = new QLineSeries();
-series->add(0, 6);
-series->add(2, 4);
-...
-chartView->chart()->addSeries(series);
-chartView->chart()->createDefaultAxes();
+
