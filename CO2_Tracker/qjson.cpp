d8a95bc503ee08e2f0e4da42d4b9173cc2297a97
jaderakoton <jaderakoton@gmail.com>
2021-11-18 11:51:44 +0100

    update qjson.cpp

--- CO2_Tracker/qjson.cpp
+++ CO2_Tracker/qjson.cpp
@@ -7,6 +7,7 @@
 #include <food.hpp>
 #include <receipt.hpp>
 #include <date.hpp>
+#include <QJsonDocument>
 
 namespace
 {
@@ -24,7 +25,7 @@ namespace
 
 using namespace std;
 
-string replace(std::string& str, std::string& from, int barcode){
+string replace(string& str, const string& from, long barcode){
     //str = const std::string url("https://world.openfoodfacts.org/api/v0/product/[barcode].json");
     int index;
     stringstream ss;
@@ -35,4 +36,85 @@ string replace(std::string& str, std::string& from, int barcode){
         str.replace(index, 9, barcode_str);
     }
     return str;
+}
+
+int main()
+{
+    Food food;
+    long barcode = food.get_barcode();
+
+    const string url_("https://world.openfoodfacts.org/api/v0/product/[barcode].json");
+    string url = replace(url, "[barcode]", barcode);
+
+    CURL* curl = curl_easy_init();
+
+    // Set remote URL.
+    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+
+    // Don't bother trying IPv6, which would increase DNS resolution time.
+    curl_easy_setopt(curl, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
+
+    // Don't wait forever, time out after 10 seconds.
+    curl_easy_setopt(curl, CURLOPT_TIMEOUT, 10);
+
+    // Follow HTTP redirects if necessary.
+    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+
+    // Response information.
+    long httpCode(0);
+    std::unique_ptr<std::string> httpData(new std::string());
+
+    // Hook up data handling function.
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, callback);
+
+    // Hook up data container (will be passed as the last parameter to the
+    // callback handling function).  Can be any pointer type, since it will
+    // internally be passed as a void pointer.
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, httpData.get());
+
+    // Run our HTTP GET command, capture the HTTP response code, and clean up.
+    curl_easy_perform(curl);
+    curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &httpCode);
+    curl_easy_cleanup(curl);
+
+    if (httpCode == 200)
+    {
+        std::cout << "\nGot successful response from " << url << std::endl;
+
+        // Response looks good - done using Curl now.  Try to parse the results
+        // and print them out.
+        Json::Value jsonData;
+        Json::Reader jsonReader;
+
+        if (jsonReader.parse(*httpData.get(), jsonData))
+        {
+            std::cout << "Successfully parsed JSON data" << std::endl;
+            std::cout << "\nJSON data received:" << std::endl;
+            std::cout << jsonData.toStyledString() << std::endl;
+
+            const std::string dateString(jsonData["date"].asString());
+            const std::size_t unixTimeMs(
+                    jsonData["milliseconds_since_epoch"].asUInt64());
+            const std::string timeString(jsonData["time"].asString());
+
+            std::cout << "Natively parsed:" << std::endl;
+            std::cout << "\tDate string: " << dateString << std::endl;
+            std::cout << "\tUnix timeMs: " << unixTimeMs << std::endl;
+            std::cout << "\tTime string: " << timeString << std::endl;
+            std::cout << std::endl;
+        }
+        else
+        {
+            std::cout << "Could not parse HTTP data as JSON" << std::endl;
+            std::cout << "HTTP data was:\n" << *httpData.get() << std::endl;
+            return 1;
+        }
+    }
+    else
+    {
+        std::cout << "Couldn't GET from " << url << " - exiting" << std::endl;
+        return 1;
+    }
+
+    return 0;
 }
