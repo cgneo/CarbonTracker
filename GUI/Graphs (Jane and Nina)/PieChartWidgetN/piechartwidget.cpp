3f1774d64973d2f8011a51edc26564f935017205
nina.bilirossi <nina.bilirossi@gmail.com>
2021-11-18 10:56:52 +0100

    created the function percentages
    a function to give the proportions of each element, given their respective carbon emissions

--- GUI/Graphs (Jane and Nina)/PieChartWidgetN/piechartwidget.cpp
+++ GUI/Graphs (Jane and Nina)/PieChartWidgetN/piechartwidget.cpp
@@ -15,5 +15,30 @@ void PieChartWidget::paintEvent(QPaintEvent *)
     painter.setBrush(Qt::blue);
     painter.drawPie(size, 90*16, 120*16);
     painter.setBrush(Qt::green);
-    painter.drawPie(size, 1200*16, 270*16);
+    painter.drawPie(size, 120*16, 270*16);
+}
+
+
+double pie_chart_data(double categories, double emissions){
+//takes in an array with all the categories, as well as an array with the associated emoissions and
+//puts them together to output data that can be inputed into the pie chart function to represent it
+
+
+}
+
+double* percentage(double* emissions){
+    double tot = 0;
+    int n = sizeof(emissions)/sizeof(emissions[0]);;
+    for(int i = 0; i<n ; i++){
+          tot+=emissions[i];
+       }
+    double proportions[n];
+            //emissions/tot;
+    for(int i = 0; i<n ; i++){
+          proportions[i] = emissions[i]/tot;
+       }
+    return proportions;
+
+
+
 }
