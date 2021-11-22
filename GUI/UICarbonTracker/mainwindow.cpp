643ea2364065a034401da91ea8bafdfd8aec85ee
Daiiinnn <91255308+Daiiinnn@users.noreply.github.com>
2021-11-22 14:02:50 +0100

    the most recent update

--- GUI/UICarbonTracker/mainwindow.cpp
+++ GUI/UICarbonTracker/mainwindow.cpp
@@ -13,4 +13,25 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+//void MainWindow::quit
+
+
+
+
+
+void MainWindow::on_checkBox_clicked()
+{
+
+}
+
+void MainWindow::on_Seedsprogress_valueChanged(int value)
+{
+    ui -> Seedsprogress -> setValue(ui -> on_checkBox_clicked());
+    //connect(); --> QCheckBox
+
+}
+void MainWindow::on_checkBox_stateChanged(int arg1)
+{
+
+}
 
--- GUI/UICarbonTracker/mainwindow.h
+++ GUI/UICarbonTracker/mainwindow.h
@@ -7,6 +7,7 @@
 #include <QTextImageFormat> //to upload the trees/seeds?
 #include <QTextFrameFormat>
 #include <QSurfaceFormat> //colour, buffer size...
+#include <QPainter>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -20,7 +21,28 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_Seedsprogress_valueChanged(int value);
+
+    void on_checkBox_clicked();
+
+    void on_checkBox_stateChanged(int arg1);
+
 private:
     Ui::MainWindow *ui;
 };
 #endif // MAINWINDOW_H
+
+//class Painter : public QPainter
+//{
+//       Q_OBJECT
+//public:
+//    Painter(QPaintDevice * device);
+//    ~Painter();
+//    void drawEllipse(const QRectF & rectangle);
+//    void drawImage(const QRectF & target, const QImage & image,
+//                   const QRectF & source, Qt::ImageConversionFlags flags = Qt::AutoColor);
+//    void drawLine(const QLine & line);
+//    void drawPicture(const QPointF & point, const QPicture & picture);
+
+//};
