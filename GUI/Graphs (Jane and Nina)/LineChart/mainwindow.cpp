0aad1e8e6b8390b0542f1a3e49235ae18c0ad06e
nina.bilirossi <nina.bilirossi@gmail.com>
2021-11-22 14:46:07 +0100

    created a line chart!

---
+++ GUI/Graphs (Jane and Nina)/LineChart/mainwindow.cpp
@@ -0,0 +1,42 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    QLineSeries *series = new QLineSeries();
+
+    series->append(0, 6);
+    series->append(2, 4);
+    series->append(3, 8);
+    series->append(7, 4);
+    series->append(10, 5);
+
+
+    *series << QPointF(11, 1) << QPointF(13, 3) << QPointF(17, 6) << QPointF(18, 3)
+            << QPointF(20, 2);
+
+    QChart *chart = new QChart();
+    //chart->legend()->hide();
+    chart->addSeries(series);
+    chart->createDefaultAxes();
+    chart->setTitle("Line Chart Example");
+
+
+    chart->legend()->setVisible(true);
+    chart->legend()->setAlignment(Qt::AlignBottom);
+
+
+
+    QChartView *chartView = new QChartView(chart);
+    chartView->setRenderHint(QPainter::Antialiasing);
+    chartView->setParent(ui->horizontalFrame);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
