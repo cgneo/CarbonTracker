747ca35c2185a1b95303c1aa75a9174847e201ea
jaderakoton <jaderakoton@gmail.com>
2021-11-25 10:51:40 +0100

    update base_consumption.cpp

--- CO2_Tracker/base_consumption.cpp
+++ CO2_Tracker/base_consumption.cpp
@@ -8,3 +8,19 @@ Base_Consumption::Base_Consumption( int userId)
     base_consumption = calculate_base_consumption();//Args missing
 
 }
+
+Base_Consumption::~Base_Consumption(){
+    delete &userId;
+    delete &base_consumption;
+}
+
+int const Base_Consumption::get_userId(){
+    return userId;
+}
+int const Base_Consumption::get_consumptionId(){
+    return consumptionId;
+}
+int const Base_Consumption::get_base_consumptionId(){
+    return base_consumptionId;
+}
+
