185e73b1a433e5ade36467af9242846829d6c3a9
alexchristlieb <alexchristlieb@gmail.com>
2021-11-25 21:15:13 +0100

    Refining of receipt class

--- CO2_Tracker/receipt.cpp
+++ CO2_Tracker/receipt.cpp
@@ -2,31 +2,29 @@
 #include "date.h"
 #include <string>
 
-Receipt::Receipt(type receipt, Date *start_date, int num_people, int duration) {
+Receipt::Receipt(int userId, type receipt, Date *start_date, int num_people, int duration) {
+    this->userId = userId;
     receipt_type = receipt;
     number_of_people = num_people;
-    set_duration(duration);
+    set_duration(duration); //Only valid for food receipts, for transport "duration = 0"
     set_dates(start_date);
+
+    receiptId = 0;//To be assigned
 }
 
-Receipt::~Receipt(){};//To be constructed
+Receipt::~Receipt(){
+    delete[] date;
+    vector<Object>().swap(content); //Free memory from vector
+};//To be constructed
 
-void Receipt::set_footprint(double footprint) {
-    this->footprint = footprint;
+
+//----------Get methods--------------------------
+int Receipt::get_userId(){
+    return userId;
 }
 
-void Receipt::set_duration(int duration) {
-    this->duration = duration; //Input by user, number of days
-
-}
-
-void Receipt::set_dates(Date *date1) {
-    date[0] = *date1;
-    date[1] = *(date1->add_duration(duration));
-}
-
-void Receipt::set_number_of_people(double number_of_people) {
-    this->number_of_people = number_of_people;
+int Receipt::get_receiptId(){
+    return receiptId;
 }
 
 double Receipt::get_footprint() {
@@ -44,6 +42,36 @@ int Receipt::get_number_of_people() {
 Date* Receipt::get_dates(){
     return date;
 }
+
+//----------Set methods---------------------------
+void Receipt::set_footprint(double footprint) {
+    if (receipt_type == food){
+        for (int i = 0; i < content.size(); i++){
+            footprint += content[i].get_footprint();
+        } //Iterate through content and add footprint of each object
+    }
+    else{
+        footprint = content[0].get_footprint(); //footprint is the footprint of the transport object
+    }
+}
+
+void Receipt::set_duration(int duration) {
+    this->duration = duration; //Input by user, number of days
+
+}
+
+void Receipt::set_dates(Date *date1) {
+    date[0] = *date1;
+    date[1] = *(date1->add_duration(duration));
+}
+
+void Receipt::set_number_of_people(double number_of_people) {
+    this->number_of_people = number_of_people;
+}
+
+
+
+//----------Other methods----------------------------
 
 void Receipt::print(){
     Date *dates = get_dates();
--- CO2_Tracker/receipt.h
+++ CO2_Tracker/receipt.h
@@ -1,16 +1,18 @@
 #ifndef RECEIPT_H
 #define RECEIPT_H
 #include "date.h"
+#include "object.h"
 #include <iostream>
 #include <string>
-
+#include <vector>
+using namespace std;
 
 enum type {food, transport};
 
 class Receipt{
 
    public:
-    Receipt(type receipt, Date *start_date,
+    Receipt(int userId, type receipt, Date *start_date,
             int num_people, int duration);
     ~Receipt();
     void set_footprint(double footprint);
@@ -21,19 +23,23 @@ class Receipt{
     double get_footprint();
     double get_duration();
     int get_number_of_people();
+    int get_userId();
+    int get_receiptId();
     Date* get_dates();
 
     void print();
+    int lastId();//Retrieve last receipt id in the DB
 
    protected:
+    int userId;
     int receiptId;
     double footprint;
-    //string file;
+    string file;
     type receipt_type;
     int duration; // length in days
-    Date* date = new Date[2]; // [01012021, 31122021] represents 1/01/2021 to 31/12/2021
+    Date* date = new Date[2];
     int number_of_people;
-    int content[]; //list of barcodes from receipt
+    vector<Object> content; //list of barcodes from receipt
 };
 
 #endif // RECEIPT_H
