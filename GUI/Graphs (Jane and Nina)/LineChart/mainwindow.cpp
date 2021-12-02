9c55f53f3bdd8be474f4348cc71b76e28280f9cf
nina.bilirossi <nina.bilirossi@gmail.com>
2021-12-02 11:39:58 +0100

    editing line chart

--- GUI/Graphs (Jane and Nina)/LineChart/mainwindow.cpp
+++ GUI/Graphs (Jane and Nina)/LineChart/mainwindow.cpp
@@ -19,25 +19,95 @@ MainWindow::MainWindow(QWidget *parent)
             << QPointF(20, 2);
 
     QChart *chart = new QChart();
-    //chart->legend()->hide();
+    chart->legend()->hide();
     chart->addSeries(series);
     chart->createDefaultAxes();
     chart->setTitle("Your carbon footprint through time");
 
 
-    chart->legend()->setVisible(true);
-    chart->legend()->setAlignment(Qt::AlignBottom);
+    //chart->legend()->setVisible(true);
+    //chart->legend()->setAlignment(Qt::AlignBottom);
 
     //axisY = QValueAxis();
     //axisY.setTitleText("Loss (W)");
 
-<<<<<<< Updated upstream
     chart->setAnimationOptions(QChart::SeriesAnimations);
     chart->setTheme(QChart::ChartThemeBrownSand);
 
-=======
+    // Customize series
+    QPen pen(QRgb(0xfdb157));
+    pen.setWidth(5);
+    series->setPen(pen);
+
+    // Customize chart title
+    QFont font;
+    font.setPixelSize(18);
+    chart->setTitleFont(font);
+    chart->setTitleBrush(QBrush(Qt::white));
+
+    // Customize chart background
+    QLinearGradient backgroundGradient;
+    backgroundGradient.setStart(QPointF(0, 0));
+    backgroundGradient.setFinalStop(QPointF(0, 1));
+    backgroundGradient.setColorAt(0.0, QRgb(0xd2d0d1));
+
+    chart->setBackgroundBrush(backgroundGradient);
+
+    // Customize plot area background
+    QLinearGradient plotAreaGradient;
+    plotAreaGradient.setStart(QPointF(0, 1));
+    plotAreaGradient.setFinalStop(QPointF(1, 0));
+    plotAreaGradient.setColorAt(0.0, QRgb(0x555555));
+    plotAreaGradient.setColorAt(1.0, QRgb(0x55aa55));
+    plotAreaGradient.setCoordinateMode(QGradient::ObjectBoundingMode);
+    chart->setPlotAreaBackgroundBrush(plotAreaGradient);
+    chart->setPlotAreaBackgroundVisible(true);
+
+
+    QCategoryAxis *axisX = new QCategoryAxis();
+    QCategoryAxis *axisY = new QCategoryAxis();
+
+    // Customize axis label font
+    QFont labelsFont;
+    labelsFont.setPixelSize(12);
+    axisX->setLabelsFont(labelsFont);
+    axisY->setLabelsFont(labelsFont);
+
+    // Customize axis colors
+    QPen axisPen(QRgb(0xd18952));
+    axisPen.setWidth(2);
+    axisX->setLinePen(axisPen);
+    axisY->setLinePen(axisPen);
+
+    // Customize axis label colors
+    QBrush axisBrush(Qt::white);
+    axisX->setLabelsBrush(axisBrush);
+    axisY->setLabelsBrush(axisBrush);
+
+    // Customize grid lines and shades
+    axisX->setGridLineVisible(false);
+    axisY->setGridLineVisible(false);
+    axisY->setShadesPen(Qt::NoPen);
+    axisY->setShadesBrush(QBrush(QColor(0x99, 0xcc, 0xcc, 0x55)));
+    axisY->setShadesVisible(true);
+
+    axisX->append("low", 10);
+    axisX->append("optimal", 20);
+    axisX->append("high", 30);
+    axisX->setRange(0, 30);
+
+    axisY->append("low", 10);
+    axisY->append("med", 20);
+    axisY->append("high", 30);
+    axisY->setRange(0, 30);
+
+    chart->addAxis(axisX, Qt::AlignBottom);
+    chart->addAxis(axisY, Qt::AlignLeft);
+    series->attachAxis(axisX);
+    series->attachAxis(axisY);
+
+
     chart->setTheme(QChart::ChartThemeBrownSand);
->>>>>>> Stashed changes
     QChartView *chartView = new QChartView(chart);
     chartView->setRenderHint(QPainter::Antialiasing);
     chartView->setParent(ui->horizontalFrame);
