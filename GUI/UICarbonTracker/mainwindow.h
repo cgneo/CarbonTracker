aa533dbfd817b1c28a5d093ad5903c9aece0d896
Daiiinnn <91255308+Daiiinnn@users.noreply.github.com>
2021-12-05 13:59:00 +0100

    progress bar depends on the daily checkbox

--- GUI/UICarbonTracker/mainwindow.h
+++ GUI/UICarbonTracker/mainwindow.h
@@ -24,7 +24,15 @@ public:
 private slots:
     void on_Seedsprogress_valueChanged(int value);
 
-    void on_pushButton_clicked();
+    void on_Surveybutton_clicked();
+
+    void on_Scanbutton_clicked();
+
+    void on_daily_challenge_1_stateChanged(int arg1);
+
+     void on_daily_challenge_2_stateChanged(int arg1);
+     void on_daily_challenge_3_stateChanged(int arg1);
+     void on_daily_challenge_4_stateChanged(int arg1);
 
 private:
     Ui::MainWindow *ui;
