a1c1117e83de8d5d1b9a57e6b7ed8c890bebd09c
Catalina-13 <71933521+Catalina-13@users.noreply.github.com>
2021-12-09 10:39:05 +0100

    Profile Picture Request
    Created a button "Choose picture" that, once clicked, opens a pop-up tab where the user selects a picture from their computer and use it as a profile picture on the app.

--- CO2_Tracker/mainwindow.h
+++ CO2_Tracker/mainwindow.h
@@ -2,7 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
+#include <QFileDialog>
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -14,6 +14,9 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private slots:
+    void on_button_image_clicked();
 
 private:
     Ui::MainWindow *ui;
