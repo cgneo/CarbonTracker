e999fd495ead681d5a6de84d73f48402cb552a43
Catalina-13 <71933521+Catalina-13@users.noreply.github.com>
2021-12-12 21:56:22 +0100

    Created a function that checks all the functions in the User class

--- CO2_Tracker/tests.cpp
+++ CO2_Tracker/tests.cpp
@@ -2,6 +2,7 @@
 #include "date.h"
 #include "random"
 #include "json.h"
+#include "user.h"
 #include <iostream>
 
 using namespace std;
@@ -11,6 +12,7 @@ Tests::Tests()
     test_add_duration();
 }
 
+// Date
 void Tests::test_add_duration(){
     try{
         for (int i = 0; i++; i < 10){
@@ -21,7 +23,7 @@ void Tests::test_add_duration(){
             fecha.set_date(day, month, 2020);
 
             Date *new_fecha = fecha.add_duration(rand()%100);
-            fecha.is_valid(); //Test if the date is valid
+            fecha.is_valid(); // Test if the date is valid
         }
     }
     catch (...){
@@ -30,7 +32,21 @@ void Tests::test_add_duration(){
     cout << "Test: Add duration passed" << endl;
 }
 
+// User
+void test_user(User *user){ // need to change this test
+    try{
+          user->name_is_valid(); // Test if the name is valid
+          user->birthday_is_vaild(); //this line is wrong, need to att a get_birthday() function
+          user->email_is_valid();
+          user->seeds_are_valid();
+          user->living_partners_are_valid();
+    }
+    catch (...){
+        throw invalid_argument("Something went wrong...");
+    }
+    cout << "Test: Set username passed" << endl;
 
+}
 
 //---------------------Json------------------------------
 
