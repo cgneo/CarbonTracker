e9e22126aa4e6759fda8b8f8908e501f52808276
CyrineBenTanfous <cyrine.ben-tanfous@polytechnique.edu>
2021-12-16 16:07:37 +0100

    changes

--- CO2_Tracker/mainwindow.cpp
+++ CO2_Tracker/mainwindow.cpp
@@ -25,6 +25,8 @@ MainWindow::MainWindow(QWidget *parent)
 
 
 
+
+
     opacity_effect_1 = new QGraphicsOpacityEffect(ui -> daily_challenge_1);
     ui -> daily_challenge_1 -> setGraphicsEffect(opacity_effect_1);
     opacity_effect_1 -> setOpacity(1);
