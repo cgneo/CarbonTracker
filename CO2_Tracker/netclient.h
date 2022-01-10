2ed18f7dcde6ee0a3f1996e56b77ce9f6ad2c091
alexchristlieb <alexchristlieb@gmail.com>
2022-01-10 23:19:05 +0100

    Retrieving commit history

---
+++ CO2_Tracker/netclient.h
@@ -0,0 +1,42 @@
+#ifndef NETCLIENT_H
+#define NETCLIENT_H
+
+#include <QAbstractSocket>
+#include <QDebug>
+#include <QFile>
+
+#include <QHostAddress>
+
+#include <QMetaType>
+#include <QString>
+#include <QStandardPaths>
+#include <QTcpSocket>
+
+class netclient : public QObject
+{
+    Q_OBJECT
+
+public:
+    void startClient();
+    explicit netclient();
+    ~netclient();
+    void readSocket();
+    void on_pushButton_sendMessage_clicked(QString& str);
+
+signals:
+    void newMessage(QString);
+
+private slots:
+
+    void discardSocket();
+    void displayError(QAbstractSocket::SocketError socketError);
+
+    void displayMessage(const QString& str);
+
+    void on_pushButton_sendAttachment_clicked();
+
+private:
+    QTcpSocket* socket;
+};
+
+#endif // NETCLIENT_H
